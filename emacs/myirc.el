9319e6c11362d41c6d9fea6e86f4c2cda7867f45
BVK Chaitanya <bvkchaitanya@gmail.com>
2011-10-16 15:57:44 -0700

    Removed IRC auth information.

--- emacs/myirc.el
+++ emacs/myirc.el
@@ -5,6 +5,3 @@
 
 ;; Show alerts on mode-line when messages are addressed to me
 (add-hook 'rcirc-mode-hook (lambda () (rcirc-track-minor-mode 1)))
-
-;; Automatically identify myself
-(setq rcirc-authinfo '(("freenode" nickserv "bvk" "helloworld")))
