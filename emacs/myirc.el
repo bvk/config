500c76cf7232395b806b522e7e496968b55a47ed
BVK Chaitanya <bvk@devbox.local>
2016-06-18 20:53:03 -0700

    emacs: hide join, disconnect and quit messages in erc.

--- emacs/myirc.el
+++ emacs/myirc.el
@@ -2,6 +2,7 @@
 
 ;; Do not display JOIN, QUIT, etc. messages
 (add-hook 'rcirc-mode-hook (lambda () (rcirc-omit-mode)))
+(setq erc-hide-list '("JOIN" "PART" "QUIT"))
 
 ;; Show alerts on mode-line when messages are addressed to me
 (add-hook 'rcirc-mode-hook (lambda () (rcirc-track-minor-mode 1)))
