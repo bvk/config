43eefea4a35abce870c2d6d0eca9927fc38e1a51
BVK Chaitanya <bvk@iron>
2017-11-23 15:30:01 -0800

    backup: added zfs-backup.sh script for taking backups easily.

---
+++ zfs-backup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Import the zpool from any file in /mnt directory using the following example:
+#
+#  sudo zpool import -d /mnt data
+#
+
+DIRS="$DIRS $HOME/Books"
+DIRS="$DIRS $HOME/Documents"
+DIRS="$DIRS $HOME/Documents"
+DIRS="$DIRS $HOME/Junk"
+DIRS="$DIRS $HOME/Maildir"
+DIRS="$DIRS $HOME/Music"
+DIRS="$DIRS $HOME/Notes"
+DIRS="$DIRS $HOME/Office"
+DIRS="$DIRS $HOME/Papers"
+DIRS="$DIRS $HOME/Pictures"
+DIRS="$DIRS $HOME/Pool"
+DIRS="$DIRS $HOME/Standards"
+DIRS="$DIRS $HOME/Study"
+DIRS="$DIRS $HOME/Work"
+DIRS="$DIRS $HOME/config"
+DIRS="$DIRS $HOME/default.org"
+DIRS="$DIRS $HOME/opt"
+DIRS="$DIRS $HOME/zion.sh"
+
+NOW=$(date +%Y-%m-%d-%H-%M-%S)
+rsync -a $DIRS /data/backup
+sudo zfs snapshot data/backup@$NOW
