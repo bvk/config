8efc35831c260ca003c75b10aca05b1edc166f18
BVK Chaitanya <bvk@zion.sh>
2018-04-08 19:36:53 -0700

    emacs: cleaned up the default dir-locals.el file.

--- emacs/dot_dir_locals.el
+++ emacs/dot_dir_locals.el
@@ -1,66 +1,55 @@
 ;; This file defines .dir-locals.el for all projects.  Users are
 ;; expected to link $HOME/.dir-locals.el to this file.
 
-(("zion.sh/code.repo"
- . ((nil
-     . ((tab-width . 2)
-        (fill-column . 79)
-	(eval
-	 . (set (make-local-variable 'compile-command)
-		"make -k -C ~/zion.sh/code.repo check"))))
+((nil . ((tab-width . 2)
+	 (fill-column . 79)))
 
-    (c++-mode
-     . ((fill-column . 79)
-        (mode . gtags)
-        (mode . electric-indent)
-        (eval . (progn
-                  (google-set-c-style)
-                  (add-hook 'before-save-hook
-                            'delete-trailing-whitespace nil t)))))
+ (c++-mode . ((mode . electric-indent)
+	      (mode . gtags)
+              (eval . (progn
+			(google-set-c-style)
+			(add-hook 'before-save-hook
+				  'delete-trailing-whitespace nil t)))))
 
-    (protobuf-mode
-     . ((tab-width . 2)
-        (indent-tabs-mode . nil)))
+ (go-mode . ((eval . (add-hook 'before-save-hook 'gofmt-before-save))))
 
-    (js-mode
-     . ((js-indent-level . 2)
-	 (tab-width . 2)
-        (indent-tabs-mode . nil)))
+ (protobuf-mode . ((tab-width . 2)
+		   (indent-tabs-mode . nil)))
 
-    (go-mode
-     . ((eval
-         . (add-hook 'before-save-hook 'gofmt-before-save))))))
+ (js-mode . ((js-indent-level . 2)
+	     (tab-width . 2)
+	     (indent-tabs-mode . nil)))
 
- ("cloudsimple/storelink.repo"
+ ;;
+ ;; Directory path specific settings
+ ;;
+
+ ("zion.sh/code.repo"
   . ((nil
       . ((tab-width . 2)
-	 (fill-column . 79)
+         (fill-column . 79)
 	 (eval
 	  . (set (make-local-variable 'compile-command)
-		 "make -C ~/cloudsimple/storelink.repo builder-all"))))
+		 "make -k -C ~/zion.sh/code.repo check"))))
 
      (c++-mode
       . ((fill-column . 79)
-	 (mode . gtags)
-	 (mode . electric-indent)
-	 (eval . (progn
-		   (google-set-c-style)
-		   (add-hook 'before-save-hook
-			     'delete-trailing-whitespace nil t)))))
+         (mode . gtags)
+         (mode . electric-indent)
+         (eval . (progn
+                   (google-set-c-style)
+                   (add-hook 'before-save-hook
+                             'delete-trailing-whitespace nil t)))))
 
      (protobuf-mode
       . ((tab-width . 2)
-	 (indent-tabs-mode . nil)))
+         (indent-tabs-mode . nil)))
+
+     (js-mode
+      . ((js-indent-level . 2)
+	 (tab-width . 2)
+         (indent-tabs-mode . nil)))
 
      (go-mode
       . ((eval
-	  . (add-hook 'before-save-hook 'gofmt-before-save))))))
-
- ("cloudsimple/control-plane.repo"
-  . ((nil
-      . ((tab-width . 2)
-	 (fill-column . 79)))
-
-     (go-mode
-      . ((eval
-	  . (add-hook 'before-save-hook 'gofmt-before-save)))))))
+          . (add-hook 'before-save-hook 'gofmt-before-save)))))))
